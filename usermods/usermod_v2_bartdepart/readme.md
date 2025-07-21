9b3aa2c3a2f82dd6d27ce900793f4dc8a0df9937
Ken Sedgwick <ken@bonsai.com>
2025-07-21 13:03:07 -0700

    add bartdepart usermod

---
+++ usermods/usermod_v2_bartdepart/readme.md
@@ -0,0 +1,11 @@
+# BARTdepart
+
+This usermod drives a strip LED and shows estimated departure times
+for Bay Area Rapid Transit (BART) trains.
+
+## Installation
+
+Add the following to your active environment in `platformio_override.ini`:
+```
+custom_usermods = usermod_v2_bartdepart
+```
---
+++ usermods/usermod_v2_bartdepart/usermod_v2_bartdepart.cpp
@@ -0,0 +1,121 @@
+#include "usermod_v2_bartdepart.h"
+#include <cstdint>
+
+const char CFG_NAME[] = "BartDepart";
+const char CFG_ENABLED[] = "Enabled";
+const char CFG_UPDATE_SECS[] = "UpdateSecs";
+const char CFG_API_KEY[] = "ApiKey";
+const char CFG_API_URL[] = "ApiUrl";
+
+const uint16_t SAFETY_DELAY_MSEC = 5000;
+
+static BartDepart bartdepart_usermod;
+REGISTER_USERMOD(bartdepart_usermod);
+
+static bool safetyWaitDone = false;
+static unsigned long startTs;
+
+BartDepart::~BartDepart() { /* nothing */ }
+
+void BartDepart::setup() {
+  // NOTE - if you are using UDP logging the DEBUG_PRINTLN in this
+  // routine will likely not show up because this is prior to WiFi
+  // being up.
+
+  // NOTE - it's a really bad idea to crash or deadlock in this
+  // method; you won't be able to use OTA update and will have to
+  // resort to a serial connection to unbrick your controller ...
+
+  DEBUG_PRINTLN(F("BartDepart::setup starting"));
+
+  //Serial.begin(115200);
+
+  // Print version number
+  DEBUG_PRINT(F("BartDepart version: "));
+  DEBUG_PRINTLN(BARTDEPART_VERSION);
+
+  // Start a nice chase so we know its booting
+  DEBUG_PRINTLN(F("BartDepart::showBooting"));
+  showBooting();
+
+  client.setInsecure();	// don't validate certs
+
+  startTs = millis();
+
+  DEBUG_PRINTLN(F("BartDepart::setup finished"));
+}
+
+void BartDepart::loop() {
+
+  // FIXME: safety delay to allow us to offMode before impending crash
+  if (!safetyWaitDone && millis() - startTs >= SAFETY_DELAY_MSEC) {
+    safetyWaitDone = true;
+  }
+
+  if (safetyWaitDone && enabled && !offMode) {
+    if (millis() - lastCheck >= updateSecs * 1000) {
+      fetchData();
+      lastCheck = millis();
+    }
+  }
+}
+
+// This function is called by WLED when the USERMOD config is read
+bool BartDepart::readFromConfig(JsonObject& root) {
+  // Attempt to retrieve the nested object for this usermod
+  JsonObject top = root[FPSTR(CFG_NAME)];
+  bool configComplete = !top.isNull();  // check if the object exists
+
+  // Retrieve the values using the getJsonValue function for better error handling
+  configComplete &= getJsonValue(top[FPSTR(CFG_ENABLED)], enabled, enabled);
+  configComplete &= getJsonValue(top[FPSTR(CFG_UPDATE_SECS)], updateSecs, updateSecs);
+  configComplete &= getJsonValue(top[FPSTR(CFG_API_KEY)], apiKey, apiKey);
+  configComplete &= getJsonValue(top[FPSTR(CFG_API_URL)], apiUrl, apiUrl);
+
+  return configComplete;
+}
+
+// This function is called by WLED when the USERMOD config is saved in the frontend
+void BartDepart::addToConfig(JsonObject& root) {
+  // Create a nested object for this usermod
+  JsonObject top = root.createNestedObject(FPSTR(CFG_NAME));
+
+  // Write the configuration parameters to the nested object
+  top[FPSTR(CFG_ENABLED)] = enabled;
+  top[FPSTR(CFG_UPDATE_SECS)] = updateSecs;
+  top[FPSTR(CFG_API_KEY)] = apiKey;
+  top[FPSTR(CFG_API_URL)] = apiUrl;
+
+  if (enabled==false)
+    // Unfreeze the main segment after disabling the module
+    strip.getMainSegment().freeze=false;
+}
+
+void BartDepart::fetchData() {
+  DEBUG_PRINTLN(F("BartDepart::fetchData starting"));
+  showLoading();
+  DEBUG_PRINTLN(F("BartDepart::fetchData finished"));
+}
+
+void BartDepart::showBooting() {
+  Segment& seg = strip.getMainSegment();
+  seg.setMode(28); // Set to chase
+  seg.speed = 200;
+  seg.intensity = 255;
+  seg.setPalette(128);
+  seg.setColor(0, 0x404060);
+  seg.setColor(1, 0x000000);
+  seg.setColor(2, 0x303040);
+}
+
+void BartDepart::showLoading() {
+  Segment& seg = strip.getMainSegment();
+  seg.setMode(28); // Set to chase
+  seg.speed = 200;
+  seg.intensity = 255;
+  seg.setPalette(128);
+  seg.setColor(0, 0x604040);
+  seg.setColor(1, 0x000000);
+  seg.setColor(2, 0x403030);
+}
+
