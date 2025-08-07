6be2b2bce2d3e6007b6c53f3bcf2916f4e9a1af9
Ken Sedgwick <ken@bonsai.com>
2025-08-07 16:27:03 -0700

    fix bartdepart utcOffset

--- usermods/usermod_v2_bartdepart/util.h
+++ usermods/usermod_v2_bartdepart/util.h
@@ -7,6 +7,6 @@
  * @brief  Returns the current wall‑clock time (seconds since Unix epoch),
  *         as driven by WLED’s SNTP client.  Will be 0 until the first sync.
  */
-inline time_t now() {
-  return ::localTime;
+inline time_t time_now() {
+  return ::localTime - utcOffsetSecs; // convert to UTC
 }
